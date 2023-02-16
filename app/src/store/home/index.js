6fc490305063b0fe9e7ba5601496a97cff0d4144
Get67 <a1099668033@qq.com>
2023-02-16 17:13:01 +0800

    2023年2月16日17:12:55

--- app/src/store/home/index.js
+++ app/src/store/home/index.js
@@ -1,15 +1,35 @@
 //home 
 
+import { reqCategoryList } from '@/api'
+
 const state = {
-    a:1
+    //state 中的数据 默认初始值 别乱写 
+    categoryList:[],
 };
 
-const mutations = {};
+const mutations = {
+    CATEGORYLIST(state, categoryList) {
+        state.categoryList = categoryList;
 
-const actions = {};
+    }
 
 
-const getters ={};
+};
+
+const actions = {
+    async categoryList({commit}) {
+        //通过api里的接口函数调用 向服务器发请求 获取数据
+        let result = await reqCategoryList();
+        if (result.code == 200) {
+            commit("CATEGORYLIST", result.data)
+        }
+
+
+    }
+};
+
+
+const getters = {};
 
 export default {
     state,
