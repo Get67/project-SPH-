d67e907f250c1723db55d5c6b1aeaebb9f787650
Get67 <a1099668033@qq.com>
2023-02-25 21:25:34 +0800

    2023年2月25日21:25:29

--- app/src/store/detail/index.js
+++ app/src/store/detail/index.js
@@ -21,7 +21,7 @@ const actions = {
         //加入购物车返回的解构
         //加入购物车以后（发请求），前台将参数带给服务器
         //服务器写入数据成功，并没有返回其他的数据，只是返回code=200，代表这次操作成功
-        //因为服务器没有返回其余的数据  所以不用三连环  把数据存储进仓库中
+        //因为服务器没有返回其余的数据  所以不用三连环  把数据存储进仓库中.
         let result = await reqAddOrUpdateShopCart(skuId, skuNum)
         console.log(result);
     }
